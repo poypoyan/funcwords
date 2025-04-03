dbcd25ff7f5bb557a32fd00a78c72dd3c4719561
poypoyan <srazin123@gmail.com>
2025-04-04 05:14:52 +0800

    Cleanup container

--- fwphl/urls.py
+++ fwphl/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(os.getenv('DJANGO_ADMIN_LOGIN'), admin.site.urls),
     path('', include('fwapp.urls_app'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
