ddf610b301b14b65733cfde39dca68f47fea5e9f
ThisPC <srazin123@gmail.com>
2025-09-03 23:04:16 +0800

    Modif form fields

--- fwapp/forms.py
+++ fwapp/forms.py
@@ -6,5 +6,8 @@ class SearchForm(forms.Form):
 
     _TYPES = (('term', 'Term'), ('language', 'Language'), ('category', 'Category'))
 
-    q = forms.CharField(max_length=100, required=True, label='')
-    t = forms.ChoiceField(choices=_TYPES, initial='term', label='')
+    q = forms.CharField(max_length=100, required=True,
+        widget=forms.TextInput(
+            attrs={'size': 30, 'autocomplete': 'off'},
+        ))
+    t = forms.ChoiceField(choices=_TYPES, initial='term')
