c4a5088ffdb06e432ced30cfbaf13bcd2f78e27b
ThisPC <srazin123@gmail.com>
2025-11-08 23:19:08 +0800

    Remove JS in nav dropdown

--- fwapp/static/functions.js
+++
@@ -1,8 +0,0 @@
-function toggleBurger(source) {
-  let x = document.getElementById("topnav");
-  if (x.className === "topnav" && source === 0) {
-    x.className += " showdropdown";
-  } else {
-    x.className = "topnav";
-  }
-}
